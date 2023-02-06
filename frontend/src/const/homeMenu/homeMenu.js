e67cde90398c228ca06366f8c73093dff29e1c62
Daniel Valderrama <danivva95@gmail.com>
2023-02-06 20:47:39 +0100

    added views until header

--- frontend/src/const/homeMenu/homeMenu.js
+++ frontend/src/const/homeMenu/homeMenu.js
@@ -1,3 +1,9 @@
+// ROUTES
 export const HOME = "/"
 export const SIGNIN = "signin"
 export const LOGIN = "login"
+
+// LABELS
+export const HOME_LABEL = "HOME"
+export const SIGIN_LABEL = "SIGNIN"
+export const LOGIN_LABEL = "LOGIN"
