44c67df15f1c8b42024a85cb7066fe90cd5550ce
Daniel Valderrama Arrabal <danivva95@gmail.com>
2023-02-08 00:20:32 +0100

    added dropdown for navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -22,71 +22,46 @@ import {
 import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
-    <nav>
-      <div className="d-flex justify-content-center">
-        <ul className="list-unstyled d-inline-flex bg-white rounded">
-          <li className="btn btn-light btn-lg p-2">
-            <Link className="text-dark text-decoration-none" to={HOME}>
+<>
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <li className="btn btn-light btn-lg p-2">
+            <Link className="text-dark navbar-brand text-decoration-none" to={HOME}>
               <span>{HOME_LABEL}</span>
             </Link>
           </li>
-
-          <li className="btn btn-light btn-lg p-2">
-            <Link className="text-dark text-decoration-none" to={LOGIN}>
-              <span>{LOGIN_LABEL}</span>
-            </Link>
-          </li>
-
-          <li className="btn btn-light btn-lg p-2">
-            <Link className="text-dark text-decoration-none" to={NOTICE}>
-              <span>{NOTICE_LABEL}</span>
-            </Link>
-          </li>
-
-          <li className="btn btn-light btn-lg p-2">
-            <Link className="text-dark text-decoration-none" to={SHIFT}>
-              <span>{SHITF_LABEL}</span>
-            </Link>
-          </li>
-          <li className="btn btn-light btn-lg p-2">
-            <Link className="text-dark text-decoration-none" to={ATTENDANCE}>
-              <span>{ATTENDANCE_LABEL}</span>
-            </Link>
-          </li>
-          <li className="btn btn-light btn-lg p-2">
-            <Link className="text-dark text-decoration-none" to={EVENTS}>
-              <span>{EVENTS_LABEL}</span>
-            </Link>
-          </li>
-
-          <button
-            className="btn btn-light btn-lg p-2"
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="btn-light btn-lg p-2 nav-item">
+          <Link className="nav-link active" aria-current="page" to={LOGIN}>{LOGIN_LABEL}</Link>
+        </li>
+        <li className="btn-light btn-lg p-2 nav-item">
+          <Link className="nav-link active" aria-current="page" to={NOTICE}>{NOTICE_LABEL}</Link>
+        </li>
+        <li className="btn-light btn-lg p-2 nav-item dropdown">
+          <span className="nav-link active" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             {ADMIN_LABEL}
-          </button>
+          </span>
           <ul className="dropdown-menu">
-            <li className="dropdown-item">
-              <Link className="text-dark text-decoration-none" to={SEARCH}>
-                <span>{SEARCH_LABEL}</span>
-              </Link>
-            </li>
-            <li className="dropdown-item">
-              <Link className="text-dark text-decoration-none" to={MANAGE}>
-                <span>{MANAGE_LABEL}</span>
-              </Link>
-            </li>
-
-            <li className="dropdown-item">
-              <Link className="text-dark text-decoration-none" to={MANAGE_EVENTS}>
-                <span>{MANAGE_EVENTS_LABEL}</span>
-              </Link>
-            </li>
+            <li><Link className="dropdown-item" to={SEARCH}>{SEARCH_LABEL}</Link></li>
+            <li><Link className="dropdown-item" to={MANAGE}>{MANAGE_LABEL}</Link></li>
+            <li><Link className="dropdown-item" to={MANAGE_EVENTS}>{MANAGE_EVENTS_LABEL}</Link></li>
+            <li><hr className="dropdown-divider"/></li>
+            <li><a className="dropdown-item" href="#">Logout</a></li>
           </ul>
-        </ul>
-      </div>
-    </nav>
+        </li>
+        
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </>
   );
 }
