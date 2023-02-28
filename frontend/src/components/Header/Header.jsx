67d8ed75f674b0920ed528091cfa9218ee04a38f
Daniel Valderrama Arrabal <danivva95@gmail.com>
2023-02-28 13:30:18 +0100

    fixed events update, deleting branch

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,39 +6,44 @@ import {
   SIGNIN,
   SIGIN_LABEL,
   ADMIN_LABEL,
-  SEARCH
+  SEARCH,
 } from "../../const/homeMenu/homeMenu";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import logo from "../../assets/Images/avoi.png";
 
-import {useCheckLoginContext} from "../../contexts/AuthContext/loginContext"
+import { useCheckLoginContext } from "../../contexts/AuthContext/loginContext";
 
 export default function Header() {
-  const {authorization, logout} = useCheckLoginContext();
+  const { authorization, logout } = useCheckLoginContext();
   return (
     <>
-      <h1>Header</h1>
-      <li className="btn-light btn-lg p-2 nav-item dropdown">
-                <span
-                  className="nav-link active"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  {ADMIN_LABEL}
-                </span>
-                <ul className="dropdown-menu">
-                  
-                  {authorization.email ? (
-                    <button className="dropdown-item" onClick={logout}>
-                      Logout
-                    </button>
-                  ) : (
-                    <Link className="dropdown-item" to={LOGIN}>
-                      Login
-                    </Link>
-                  )}
-                </ul>
-              </li>
+      <div className="d-flex justify-content-evenly">
+        <img src={logo} className="justify-content-center"  />
+    <ul className="unstyled">
+
+      <li className="btn-light btn-lg list-unstyled p-2 nav-item dropdown">
+        <span
+          className="nav-link active"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          {ADMIN_LABEL}
+        </span>
+        <ul className="dropdown-menu">
+          {authorization.email ? (
+            <button className="dropdown-item" onClick={logout}>
+              Logout
+            </button>
+          ) : (
+            <Link className="dropdown-item" to={LOGIN}>
+              Login
+            </Link>
+          )}
+        </ul>
+      </li>
+    </ul>
+      </div>
     </>
   );
 }
--- frontend/src/views/EventID/EventIdView.jsx
+++ frontend/src/views/EventID/EventIdView.jsx
@@ -1,31 +1,63 @@
-import { useState } from "react"
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-export default function EventIdView({event,updatedEvent, setUpdateEvent}) {
-
+export default function EventIdView() {
   const [toggle, setToggle] = useState(false);
+  const [changeEvent, setChangeEvent] = useState(false);
+  const { id } = useParams(); //Lo llamamos id porque en App.jsx el path lo llamamos ":id"
+  const [event, setEvent] = useState(null);
+  const [updatedEvent, setUpdatedEvent] = useState({
+    titulo: "",
+    descripcion: "",
+    fecha: "",
+  });
 
   function handleToggleTrue(e) {
     e.preventDefault();
-    setToggle(true)
+    setToggle(true);
   }
 
   function handleUpdate(e) {
     e.preventDefault();
     const newEvent = {
-      [e.target.name]:e.target.value
-
-    }
-    setUpdateEvent(newEvent)
+      ...updatedEvent,
+      [e.target.name]: e.target.value,
+    };
+    setUpdatedEvent(newEvent);
   }
 
-    return (
+  useEffect(function () {
+    async function fetchEvent() {
+      const response = await fetch(`http://localhost:3000/event/${id}`);
+      const data = await response.json();
+      setEvent(data);
+    }
+    fetchEvent();
+  }, [changeEvent]);
+
+  async function updateEvent(e) {
+    e.preventDefault();
+    const event = await fetch(`http://localhost:3000/event/update/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(updatedEvent),
+    });
+    if (event.status === 200) {
+      setToggle(!toggle);
+      setChangeEvent(!changeEvent)
+    } else {
+      alert("error");
+      console.log(event.status);
+    }
+  }
+
+  return (
+    <>
+      <h1>Esto es Evento Detalle</h1>
+
+      {event && !toggle && (
         <>
-        <h1>Esto es Evento Detalle</h1>
-
-
-        {event && !toggle && (
-          <>
-        <div className="card" style={{ width: "18rem" }} key={event.id}>
+          <div className="card" style={{ width: "18rem" }} key={event.id}>
             <img
               src={`http://127.0.0.1:3000/${event.imagen}`}
               className="card-img-top"
@@ -37,13 +69,12 @@ export default function EventIdView({event,updatedEvent, setUpdateEvent}) {
               <p className="card-text">{event.fecha.split("T")[0]}</p>
             </div>
           </div>
-            <button onClick={handleToggleTrue}>Editar</button>
-            </>
-        ) }
-        
-        {event && toggle &&(
+          <button onClick={handleToggleTrue}>Editar</button>
+        </>
+      )}
 
-          <>
+      {event && toggle && (
+        <>
           <div className="card" style={{ width: "18rem" }} key={event.id}>
             <img
               src={`http://127.0.0.1:3000/${event.imagen}`}
@@ -51,17 +82,25 @@ export default function EventIdView({event,updatedEvent, setUpdateEvent}) {
               alt="..."
             />
             <div className="card-body">
-              <input className="text-white card-title" name="titulo" placeholder={event.title} onChange={handleUpdate}></input>
-              <p className="card-text">{event.descripcion}</p>
-              <p className="card-text">{event.fecha.split("T")[0]}</p>
+              <form onSubmit={(e) => updateEvent(e)}>
+                <input
+                  className="text-white card-title"
+                  name="titulo"
+                  placeholder={event.title}
+                  value={updatedEvent.titulo}
+                  onChange={handleUpdate}
+                ></input>
+
+                <input className="card-text" name="descripcion" onChange={handleUpdate} value={updatedEvent.descripcion}>
+                </input>
+                <input type="date" className="card-text" name="fecha" onChange={handleUpdate} value={updatedEvent.fecha}>
+                </input>
+                <button type="submit">Guardar Cambios</button>
+              </form>
             </div>
           </div>
-          <button onClick={handleUpdate}>Guardar Cambios</button>
-          </>
-        )}
-
-
-
         </>
-    )
+      )}
+    </>
+  );
 }
